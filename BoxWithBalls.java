185e287a0b7ffced299e102207b1b1391b6e4534
Inaksim <91840622+Inaksim@users.noreply.github.com>
2023-04-19 19:06:35 +0300

    Update and rename Main.java to BoxWithBalls.java

--- BoxWithBalls.java
+++ BoxWithBalls.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class boxWithBalls {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, Integer> balls = new HashMap<>();
